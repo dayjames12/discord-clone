c6097137d17335816130de731329058730b507e8
James Day <jday6@mail.sfsu.edu>
2020-11-09 22:44:11 -0800

    added chat header and chat input, chat format is mostly there

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import "./Chat.css"
+import ChatHeader from "./ChatHeader.js"
+import AddCircleIcon from "@material-ui/icons/AddCircle"
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard"
+import GifIcon from "@material-ui/icons/Gif"
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions"
 
 function Chat() {
-    return <div className="chat">
-        <h2>I am the Chat screen</h2>
-        {/* <ChatHeader /> */}
-    </div>
+    return ( 
+        <div className="chat">
+            <ChatHeader />
+        
+        <div className="chat__messages">
+
+        </div>
+
+        <div className="chat__input">
+            <AddCircleIcon fontsize="large" />
+            <form>
+                <input placeholder={"Message #TESTCHANNEL"}/>
+                <button className="chat__inputButton" type="submit">
+                    Send Message
+                </button>
+            </form>
+
+            <div className="chat__inputIcons">
+                <CardGiftcardIcon fontsize="large" />
+                <GifIcon fontsize="large" />
+                <EmojiEmotionsIcon fontsize="large" />
+            </div>
+        </div>
+        
+        </div>
+    )
 }
 
 export default Chat
