339b979f7f04b766b8993e2ad3addf654f8ebcdf
James Day <jday6@mail.sfsu.edu>
2020-11-10 14:57:03 -0800

    text channels are now functional, the chat works, the chat header changes depending on the selected chat channel

--- src/Message.js
+++ src/Message.js
@@ -2,16 +2,19 @@ import { Avatar } from '@material-ui/core'
 import React from 'react'
 import "./Message.css"
 
-function Message() {
+function Message({ timestamp, message, user }) {
     return (
         <div className="message">
-            <Avatar />
+            <Avatar src={user.photo} />
             <div className="message__info">
-                <h4>dayjames
-                    <span className="message__timestamp">timestamp example</span>
+                <h4>
+                    {user.displayName}
+                    <span className="message__timestamp">
+                        {new Date(timestamp?.toDate()).toUTCString()}    
+                    </span>
                 </h4>
 
-                <p>Here is an example message</p>
+                <p>{message}</p>
             </div>
         </div>
     )
